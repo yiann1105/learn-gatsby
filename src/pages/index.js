e6fae574cac2eb8bc2c8e38ad1267d21ea7f4ad7
Lau Yi Ann <yalau@merimenkl.com>
2019-07-13 11:19:34 +0800

    add home page

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Malcolm Kee's Website</h1>
+      <p>crafted with tears and love.test</p>
+    </div>
+  );
+};
+export default HomePage;
