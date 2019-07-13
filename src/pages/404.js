8ac8c8c7a1cef3ca69432d6cfc5e78f959573126
Yi Ann <yiann1990hong@gmail.com>
2019-07-13 11:25:22 +0800

    add not found page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Man... I'm sorry for you</p>
+    </div>
+  );
+};
+export default NotFoundPage;
